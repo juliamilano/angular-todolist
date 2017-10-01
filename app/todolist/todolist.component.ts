da909afe2f4eaaa2e95fd1b66cea85455091830a
juliamilano <t_yulya97@mail.ru>
2017-10-01 22:56:42 +0300

    add directives

--- app/todolist/todolist.component.ts
+++ app/todolist/todolist.component.ts
@@ -2,6 +2,21 @@ import { Component } from "@angular/core";
 
 // Для использования относительных путей, необходимо добавить свойство moduleId и установить значение для свойства module.id
 // Данное свойство необходимо устанавливать в случае если в проекте используется загрузчик systemJS
+
+const todos = [
+    {
+        title: "Изучить JS",
+        completed: true
+    },
+    {
+        title:  "Изучить Angular 2",
+        completed: false
+    },{
+        title:  "Написать ToDoList",
+        completed: false
+    }
+];
+
 @Component({
     moduleId: module.id,
     selector: "todos",
@@ -9,5 +24,15 @@ import { Component } from "@angular/core";
     styleUrls: ["todolist.component.css"]
 })
 export class TodosComponent  {
-    tittle = `Изучить Angular 2`;
+    title = `Изучить Angular 2`;
+    todos = todos;
+    toggle(todo: any){
+        todo.comleted = !todo.completed;
+    };
+    delete(todo: any){
+        let index = this.todos.indexOf(todo);
+        if(index>-1){
+            this.todos.splice(index,1);
+        }
+    };
 }
